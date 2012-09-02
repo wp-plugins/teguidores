847f5a2a3ec4395f6e4b5a9249907f77ebd1938c
Rdemiguel <Rdemiguel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-02 16:59:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/teguidores/tags/1.0@593680 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ visual.php
@@ -0,0 +1,27 @@
+<div style="text-align: center; font-size: 10px;  ">
+ <a style="width: 80px; float: left; padding: 5px 0 5px 0; 
+ text-decoration: none;" href="http://twitter.com/wordpring" title="Síguenos en Twitter" target="_blank">
+ <img src="/wp-content/plugins/teguidores/imagenes/twitter.png" alt="Síguenos en Twitter" /><br/>
+ <?php 
+ // Modificar con tu usuario de Twitter
+ obtener_seguidores_de_twitter('WordPring'); ?>  Followers
+
+ </a>
+ <a style="width: 80px; float: left; padding: 5px 0 5px 0; text-decoration: none;" href="http://facebook.com/wordpring" title="Síguenos en Facebook" target="_blank">
+ <img src="/wp-content/plugins/teguidores/imagenes/facebook.png" alt="Síguenos en Facebook" /><br/>
+ <?php
+// Modificar con el ID de tu pagina
+ obtener_fans_de_facebook ('347064288693665'); ?> Fans
+ 
+ </a>
+ <a style="width: 80px; float: left; padding: 5px 0 5px 0; text-decoration: none;" href="http://feeds.feedburner.com/wordpring" title="Suscríbete - RSS Feed" target="_blank">
+ <img src="/wp-content/plugins/teguidores/imagenes/rss.png" alt="Suscribete RSS" /><br/>
+ <?php 
+ // Modificar con tu usuario de FeedBurner
+diww_fb_sub_value('wordpring') ?> Lectores
+
+ </a>
+ </div>
+ <div style="clear: both;">
+</div>
+
